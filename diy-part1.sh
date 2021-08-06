201d6afe5a621f0ffd05bdb8ea220b4674fbc65a
marsnego <marsnego@gmail.com>
2021-08-06 18:35:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+ 
  Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
  Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
